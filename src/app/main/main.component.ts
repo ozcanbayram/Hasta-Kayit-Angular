3301a19baebee57ff69b39cde6fd550106e3b1c5
Ozcan Bayram <ozzcanbayram@gmail.com>
2024-06-12 11:28:01 +0300

    main content box expandable by menu

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuService } from '../shared/services/menu.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
 
   isVisible: boolean = true;
+  isVisibleSecondBox: boolean = true;
+  menuVisible: boolean = false;
+
+  constructor(private menuService: MenuService) {}
+
+  ngOnInit() {
+    this.menuService.menuVisible$.subscribe(visible => {
+      this.menuVisible = visible;
+    });
+  }
 
   toggleVisibility() {
     this.isVisible = !this.isVisible;
   }
 
-  isVisibleSecondBox: boolean = true;
-
   toggleVisibilitySecondBox() {
     this.isVisibleSecondBox = !this.isVisibleSecondBox;
   }
-
 }
